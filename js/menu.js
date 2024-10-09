9fde086132cc804f25a91a74e840a1e0c132f5ad
Mikkel Jørgensen <95000922+ExManubis@users.noreply.github.com>
2024-10-09 15:10:01 +0200

    Add global navbar and burger-menu
    Added javascript for burger-menu, a global navbar and mobile + desktop
    layouts.

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+// CONSTANTS
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('nav');
+const menu = document.querySelector('.menu');
+
+// FUNCTIONS
+burger.addEventListener('click', burgerClick);
+function burgerClick() {
+    burger.classList.toggle('active');
+    nav.classList.toggle('active');
+}
+
+menu.addEventListener('click', menuClick);
+function menuClick() {
+    burger.classList.remove('active');
+    nav.classList.remove('active');
+}
