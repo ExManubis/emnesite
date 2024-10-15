e9215a5b8bef5952cec8f467c9f82ee97d4d6b4e
Mikkel Jørgensen <95000922+ExManubis@users.noreply.github.com>
2024-10-15 21:07:10 +0200

    Add javascript for login overlay
    Added initial javascript for the login overlay.

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,8 @@
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('nav');
 const menu = document.querySelector('.menu');
+const login = document.querySelector('.login');
+
 
 // FUNCTIONS
 burger.addEventListener('click', burgerClick);
@@ -14,4 +16,10 @@ menu.addEventListener('click', menuClick);
 function menuClick() {
     burger.classList.remove('active');
     nav.classList.remove('active');
+}
+
+login.addEventListener('click', loginClick);
+function loginClick() {
+    login.classList.toggle('active');
+    nav.classList.toggle('active');
 }
