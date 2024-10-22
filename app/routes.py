5700fc63152c36131c6e170598d2981559b711e1
Mikkel Jørgensen <95000922+ExManubis@users.noreply.github.com>
2024-10-22 21:03:14 +0200

    Add flask-wtf logic to login forms
    Added logic to the login forms, so that they will pass info, validate
    and show error messages to the user.

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,14 @@
 # IMPORTS
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 # FLASK ROUTES
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('index.html', title = "The Film Bucket Challenge", form = form)
