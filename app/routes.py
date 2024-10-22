e34a9f58d2e7a027ec79fb4d831b94d2fe146065
Mikkel Jørgensen <95000922+ExManubis@users.noreply.github.com>
2024-10-22 21:25:55 +0200

    Add password hashing function
    Added password hashing function and checking via werkzeug

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 # IMPORTS
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -10,5 +10,5 @@ def index():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('index.html', title = "The Film Bucket Challenge", form = form)
